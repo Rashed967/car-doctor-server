75208d3743f008f7a6abd7ea91de47f677a08873
Rashed967 <rashedhasan967@gmail.com>
2023-05-17 01:18:08 +0600

    added more code

--- index.js
+++ index.js
@@ -24,6 +24,23 @@ const client = new MongoClient(uri, {
   }
 });
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization
+  if(!authorization){
+    return res.status(402).send({error : true, message : "unauthorized access"})
+  }
+  const token = authorization.split(' ')[1]
+  jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, (error, decoded) => {
+    if(error){
+      return res.status(401).send({error : true, message : "unauthorized access"})
+    }
+    req.decoded = decoded
+    next()
+  })
+}
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -65,7 +82,13 @@ async function run() {
 
 
     // checkout 
-    app.get('/checkout', async(req, res) => {
+    app.get('/checkout',verifyJWT, async(req, res) => {
+      console.log(req.headers.authorization)
+      const decoded = req.decoded;
+      if(decoded.email !== req.query.email){
+        return res.status(403).send({eroor : true, message : "acces denied"})
+      }
+
       let query = {};
       if(req.query?.email) {
         query = {email : req.query.email}
